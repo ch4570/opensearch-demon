701d230fe2beaa7a58db22630fbd4f79d0403038
서민재 <seominjae@seominjaeui-MacBookPro.local>
2023-11-13 15:28:03 +0900

    ⭐ feat: Pipeline 정의 기능 추가

--- src/main/kotlin/com/demon/dummy/event/EventProducer.kt
+++ src/main/kotlin/com/demon/dummy/event/EventProducer.kt
@@ -2,16 +2,78 @@ package com.demon.dummy.event
 
 import com.demon.dummy.domain.DummyDto
 import jakarta.annotation.PostConstruct
+import org.opensearch.action.ingest.PutPipelineRequest
+import org.opensearch.client.RequestOptions
+import org.opensearch.client.RestHighLevelClient
+import org.opensearch.client.indices.CreateIndexRequest
+import org.opensearch.common.bytes.BytesArray
+import org.opensearch.common.settings.Settings
+import org.opensearch.common.xcontent.XContentType
+import org.opensearch.core.xcontent.MediaType
 import org.springframework.stereotype.Component
 
 @Component
 class EventProducer(
-    private val fileEventHandler: FileEventHandler
+    private val fileEventHandler: FileEventHandler,
+    private val restHighLevelClient: RestHighLevelClient,
 ) {
 
     @PostConstruct
     fun createEvent() {
+        Thread.sleep(30000L)
+        createPipeLine()
+        createIndex()
         fileEventHandler.sendRequesting(DummyDto("linuxsys"))
         fileEventHandler.sendRequesting(DummyDto("linuxauth"))
+    }
+
+    fun createIndex() {
+
+        val jsonData = """
+            {
+              "number_of_shards": 3,
+              "number_of_replicas": 2
+            }
+        """.trimIndent()
+
+        val request = CreateIndexRequest("category-log-linuxauth")
+        request.settings(Settings.builder().loadFromSource(jsonData, XContentType.JSON))
+
+        // Execute the request and get the response
+        restHighLevelClient.indices().create(request, RequestOptions.DEFAULT)
+    }
+
+    fun createPipeLine() {
+        val pipelineName = "timestamp-processor"
+        val pipelineDefinition = """
+                {
+                  "description": "Add timestamp to documents",
+                  "processors": [
+                    {
+                      "date": {
+                        "field": "_ingest.timestamp",
+                        "target_field": "@timestamp",
+                        "formats": ["ISO8601"]
+                      }
+                    },
+                    {
+                      "script": {
+                        "source": "ZonedDateTime originalTime = ZonedDateTime.parse(ctx['@timestamp']); ZonedDateTime koreaTime = originalTime.withZoneSameInstant(ZoneId.of('Asia/Seoul')); ctx['@timestamp'] = koreaTime.format(DateTimeFormatter.ofPattern('yyyy-MM-dd HH:mm:ss'));",
+                        "lang": "painless"
+                      }
+                    }
+                  ]
+                }
+                """.trimIndent()
+
+        val pipelineBytes = BytesArray(pipelineDefinition)
+        val mediaType = XContentType.JSON as MediaType
+        val pipelineRequest = PutPipelineRequest(
+            pipelineName, pipelineBytes, mediaType)
+
+        restHighLevelClient.ingest().putPipeline(
+            pipelineRequest, RequestOptions.DEFAULT
+        )
+
     }
 }
